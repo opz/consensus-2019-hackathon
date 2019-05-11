fb1d85dee336be76b8b17487aa17b3009cc76d70
opz <will.shahda@gmail.com>
2019-05-11 14:55:23 -0400

    Add getters for contracts in ContractFactory

--- contracts/ContractFactory.sol
+++ contracts/ContractFactory.sol
@@ -4,9 +4,35 @@ import './ProductContract.sol';
 
 contract ContractFactory {
     mapping(address => address[]) public sellerToContracts;
+    mapping(address => address[]) public buyerToContracts;
 
-    function createContract(string calldata name) external {
-        ProductContract pcontract = new ProductContract(msg.sender, name);
+    function createContract(string calldata name, address payable buyer)
+        external
+    {
+        require(msg.sender != buyer, 'Seller and Buyer cannot be the same address');
+
+        ProductContract pcontract = new ProductContract(
+            msg.sender,
+            buyer,
+            name
+        );
         sellerToContracts[msg.sender].push(address(pcontract));
+        buyerToContracts[msg.sender].push(address(pcontract));
+    }
+
+    function getSellerContracts(address seller)
+        external
+        view
+        returns (address[] memory)
+    {
+        return sellerToContracts[seller];
+    }
+
+    function getBuyerContracts(address buyer)
+        external
+        view
+        returns (address[] memory)
+    {
+        return buyerToContracts[buyer];
     }
 }
