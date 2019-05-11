ee27c6c2dc99d9042d7929758aa8e2384eabeea3
opz <will.shahda@gmail.com>
2019-05-11 14:55:23 -0400

    Remove optional requirement for accounts from web3 HOC

--- client/src/utils/withWeb3.js
+++ client/src/utils/withWeb3.js
@@ -5,45 +5,44 @@ import getWeb3 from './getWeb3';
  * WrappedComponent should use componentWillReceiveProps to implement web3
  * calls instead of componentDidMount.
  */
-export default function withWeb3(requireAccounts=false) {
-  return WrappedComponent => {
-    return class extends Component {
-      state = {
-        web3: null,
-        accounts: null,
-        loading: false
-      };
+export default function withWeb3(WrappedComponent) {
+  return class extends Component {
+    state = {
+      web3: null,
+      accounts: null,
+      loading: false
+    };
 
-      async componentDidMount() {
-        this.setState({ loading: true });
+    async componentDidMount() {
+      this.setState({ loading: true });
 
-        try {
-          // Get network provider and web3 instance.
-          const web3 = await getWeb3();
+      try {
+        // Get network provider and web3 instance.
+        const web3 = await getWeb3();
 
-          // Use web3 to get the user's accounts.
-          const accounts = web3 && await web3.eth.getAccounts();
+        // Use web3 to get the user's accounts.
+        const accounts = web3 && await web3.eth.getAccounts();
 
-          this.setState({ web3, accounts });
-        } catch (error) {
-          // Catch any errors for any of the above operations.
-          console.error(error);
-        }
-
-        this.setState({ loading: false });
-      };
-
-      render() {
-        const { web3, accounts, loading } = this.state;
-
-        return (
-          <WrappedComponent
-            {...this.props}
-            web3={web3}
-            accounts={accounts}
-          />
-        );
+        this.setState({ web3, accounts });
+      } catch (error) {
+        // Catch any errors for any of the above operations.
+        console.error(error);
       }
+
+      this.setState({ loading: false });
+    };
+
+    render() {
+      const { web3, accounts, loading } = this.state;
+
+      return (
+        <WrappedComponent
+          {...this.props}
+          web3={web3}
+          accounts={accounts}
+          loading={loading}
+        />
+      );
     }
   };
 }
