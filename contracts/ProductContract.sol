576e275fb906442b8b0b22f51c558cd3a16f02df
opz <will.shahda@gmail.com>
2019-05-11 14:55:23 -0400

    Implement product contract and refund escrow
    Product contract defines the terms between a buyer and seller and the
    refund escrow handles the exchange of funds.

---
+++ contracts/ProductContract.sol
@@ -0,0 +1,85 @@
+pragma solidity ^0.5.2;
+
+import './RefundEscrow.sol';
+
+contract ProductContract {
+    enum DeliveryStatus { NotYet, Delivered, Failed }
+
+    RefundEscrow private _escrow;
+
+    string public name;
+    address payable public seller;
+    address payable public buyer;
+    uint public amount;
+    bool public shipped;
+    DeliveryStatus delivered;
+
+    constructor(string memory _name) public {
+        name = _name;
+        seller = msg.sender;
+        _escrow = new RefundEscrow(seller);
+    }
+
+    modifier onlyBuyer() {
+        require(msg.sender == buyer, "User is not the Buyer");
+        _;
+    }
+    modifier onlySeller() {
+        require(msg.sender == seller, "User is not the Seller");
+        _;
+    }
+
+    function sendFunds() external onlyBuyer payable {
+        require(msg.value > 0);
+
+        amount = msg.value;
+        _escrow.deposit.value(amount)(buyer);
+    }
+
+    function setDelivered(DeliveryStatus _delivered) external onlyBuyer {
+        delivered = _delivered;
+
+        if (delivered == DeliveryStatus.Failed) {
+            _escrow.enableRefunds();
+        } else {
+            _escrow.disableRefunds();
+        }
+    }
+
+    function setShipped(bool _shipped) external onlySeller {
+        shipped = _shipped;
+    }
+
+    function setBuyer(address payable _buyer) public payable onlySeller {
+        buyer = _buyer;
+        // set buyerToContract mapping in factory
+    }
+
+    function withdrawToSeller() public onlySeller {
+        require(shipped == true);
+        require(delivered == DeliveryStatus.Delivered);
+        require(depoOf() >= amount);
+        _escrow.close();
+        _escrow.beneficiaryWithdraw();
+    }
+
+    function withdrawToBuyer() public onlyBuyer {
+        require(shipped == true);
+        require(delivered == DeliveryStatus.Failed);
+        _escrow.withdraw(buyer);
+    }
+
+    function depoOf() public view returns (uint256) {
+        return _escrow.depositsOf(buyer);
+    }
+
+    function getDetails()
+        external
+        view
+        onlyBuyer
+        onlySeller
+        returns (string memory, address, address, uint, bool, DeliveryStatus)
+    {
+        return (name, seller, buyer, amount, shipped, delivered);
+    }
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var ProductContract = artifacts.require("ProductContract");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  deployer.deploy(ProductContract, 'Test name');
 };
