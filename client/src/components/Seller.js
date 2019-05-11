993cff2120aedf725fc60941958cae1b53925a15
Ernesto Cejas <cejas@ninjacuba.com>
2019-05-11 19:55:02 -0400

    Add UI and logic to create seller contracts

--- client/src/components/Seller.js
+++ client/src/components/Seller.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import withWeb3 from "../utils/withWeb3";
+import withContract from "../utils/withContract"
 import {
   Card,
   CardBody,
@@ -11,18 +12,53 @@ import {
   MDBFreeBird,
   MDBIcon,
   MDBContainer,
+  MDBCol,
+  MDBInput,
+  MDBRow,
   Row,
   Table,
   TableBody,
   TableHead,
 } from "mdbreact";
 
+import ContractFactory from "../contracts/ContractFactory.json"
+
+// create contract. returns a promise.
+//this.props.factory.methods.createContract().send();
+
 class Seller extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      contractName: "",
+      buyerAddress: "",
       contracts: {},
     };
+
+    this.handleContractNameChange = this.handleContractNameChange.bind(this);
+    this.handleBuyerAddressChange = this.handleBuyerAddressChange.bind(this);
+    this.handleCreateContract = this.handleCreateContract.bind(this);
+  }
+
+  handleContractNameChange(e) {
+    this.setState({ contractName: e.target.value });
+  };
+
+  handleBuyerAddressChange(e) {
+    this.setState({ buyerAddress: e.target.value });
+  };
+
+  handleCreateContract(e) {
+    try {
+      this.props.factory.methods.createContract(
+        this.state.contractName,
+        this.state.buyerAddress,
+      ).send({
+        "from": this.props.accounts[0],
+      });
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   render() {
@@ -53,15 +89,34 @@ class Seller extends Component {
       <MDBContainer fluid>
         <MDBEdgeHeader color="indigo darken-3" />
         <MDBFreeBird>
-        </MDBFreeBird>
+        <MDBRow>
+            <MDBCol className="mx-auto float-none ">
+              <MDBCard className="mx-auto float-none white z-depth-1">
+                <MDBCardHeader className="py-2 px-2" color="primary-color" tag="h4"> Add a contract </MDBCardHeader>
+                <div className="card-body">
+                  <form
+                    className="needs-validation"
+                    onSubmit={this.handleCreateContract}
+                    noValidate>
+                    <div >
+                      <MDBInput size="sm" className="px-2" label="Name"
+                        required icon="user" type="email" value={this.state.contractName} onChange={this.handleContractNameChange} />
+                      <MDBInput size="sm" className="px-2" label="Buyer address"
+                        required icon="lock" onChange={this.handleBuyerAddressChange} value={this.state.buyerAddress} />
+                    </div>
+                    <div className="text-center mt-3">
+                      <MDBBtn
+                        color="primary"
+                        type="submit">
+                        Create Contract
+                      </MDBBtn>
+                    </div>
+                  </form>
+                </div>
 
-        <div className="text-center mt-3">
-          <MDBBtn
-            color="primary"
-            type="submit">
-            Create Contract
-            </MDBBtn>
-        </div>
+              </MDBCard>
+            </MDBCol>
+          </MDBRow>
 
         <hr className="my-2" />
           <Row >
@@ -82,10 +137,11 @@ class Seller extends Component {
               </Card>
             </Col>
           </Row>
+        </MDBFreeBird>
 
       </MDBContainer>
     );
   }
 }
 
-export default withWeb3(Seller);
+export default withContract(ContractFactory, 'factory')(withWeb3(Seller));
