ee2f233e2faad4aa0a87b181645b5fef496e50df
Ernesto Cejas <cejas@ninjacuba.com>
2019-05-11 19:10:53 -0400

    add mock ui for seller

--- client/src/components/Seller.js
+++ client/src/components/Seller.js
@@ -1,32 +1,91 @@
 import React, { Component } from "react";
 import withWeb3 from "../utils/withWeb3";
 import {
+  Card,
+  CardBody,
+  Col,
   MDBBtn,
   MDBCard,
   MDBCardHeader,
+  MDBEdgeHeader,
+  MDBFreeBird,
+  MDBIcon,
   MDBContainer,
+  Row,
+  Table,
+  TableBody,
+  TableHead,
 } from "mdbreact";
 
 class Seller extends Component {
-  state = {
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      contracts: {},
+    };
+  }
 
   render() {
+    let contractRows = [];
+    let index = 0;
+    for (let key in this.state.contracts) {
+      contractRows.push(
+        {
+          id: index += 1,
+          heading0: key,
+        })
+    };
+
+    const columns = [
+      {
+        label: "#",
+        field: "id",
+        sort: "asc"
+      },
+      {
+        label: "Name",
+        field: "heading0",
+        sort: "asc"
+      },
+    ];
+
     return (
       <MDBContainer fluid>
-        <MDBCard className="mx-auto float-none white z-depth-1">
-          <MDBCardHeader className="py-2 px-2" color="primary-color" tag="h4"> Account Management</MDBCardHeader>
-          <div className="card-body">
-            <MDBBtn className="px-2" block color="danger">
-              Be happy!
+        <MDBEdgeHeader color="indigo darken-3" />
+        <MDBFreeBird>
+        </MDBFreeBird>
+
+        <div className="text-center mt-3">
+          <MDBBtn
+            color="primary"
+            type="submit">
+            Create Contract
             </MDBBtn>
-          </div>
-        </MDBCard>
+        </div>
+
+        <hr className="my-2" />
+          <Row >
+            <Col md="12">
+              <Card>
+                <CardBody>
+                  <h2 className="h2-responsive pb-4">Your Accounts</h2>
+                  <Table responsive small>
+                    <TableHead
+                      columns={columns}
+                      color="primary-color"
+                      textWhite
+                    />
+                    <TableBody rows={contractRows}
+                    />
+                  </Table>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+
       </MDBContainer>
     );
   }
 }
-
-
 
 export default withWeb3(Seller);
