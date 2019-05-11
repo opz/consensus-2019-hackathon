7d595c06103ba6b10f1ebb07f9c8b2f939191106
opz <will.shahda@gmail.com>
2019-05-11 15:09:30 -0400

    Create HOC for web3 and contracts
    Using HOC for getting web3 instead of the root component allows us to
    have pages that do not need web3.
    
    Improvements to getWeb3 make it more robust.
    
    Two environment variables are used to configure the default network when
    metamask is not available:
    - REACT_APP_PROD_NETWORK
    - REACT_APP_DEV_NETWORK

--- client/src/utils/getWeb3.js
+++ client/src/utils/getWeb3.js
@@ -1,38 +1,48 @@
 import Web3 from "web3";
 
-const getWeb3 = () =>
-  new Promise((resolve, reject) => {
-    // Wait for loading completion to avoid race conditions with web3 injection timing.
-    window.addEventListener("load", async () => {
-      // Modern dapp browsers...
-      if (window.ethereum) {
-        const web3 = new Web3(window.ethereum);
-        try {
-          // Request account access if needed
-          await window.ethereum.enable();
-          // Acccounts now exposed
-          resolve(web3);
-        } catch (error) {
-          reject(error);
-        }
-      }
-      // Legacy dapp browsers...
-      else if (window.web3) {
-        // Use Mist/MetaMask's provider.
-        const web3 = window.web3;
-        console.log("Injected web3 detected.");
-        resolve(web3);
-      }
-      // Fallback to localhost; use dev console port by default...
-      else {
-        const provider = new Web3.providers.HttpProvider(
-          "http://127.0.0.1:9545"
-        );
-        const web3 = new Web3(provider);
-        console.log("No web3 instance injected, using Local web3.");
-        resolve(web3);
-      }
-    });
-  });
+const getWeb3 = async (resolve, reject) => {
+  // Modern dapp browsers...
+  if (window.ethereum) {
+    const web3 = new Web3(window.ethereum);
+    try {
+      // Request account access if needed
+      await window.ethereum.enable();
+      // Accounts now exposed
+      resolve(web3);
+    } catch (error) {
+      reject(error);
+    }
+  // Legacy dapp browsers...
+  } else if (window.web3) {
+    // Use Mist/MetaMask's provider.
+    const web3 = new Web3(window.web3.currentProvider);
+    console.log("Injected web3 detected.");
+    resolve(web3);
+  // Default provider when accounts are not required
+  } else {
+    let defaultNetwork;
+    if (process.env.NODE_ENV === 'production') {
+      defaultNetwork = process.env.REACT_APP_PROD_NETWORK;
+    } else {
+      defaultNetwork = process.env.REACT_APP_DEV_NETWORK;
+    }
+    const provider = new Web3.providers.HttpProvider(defaultNetwork);
+    const web3 = new Web3(provider);
+    console.log("No web3 instance injected, using default provider.");
+    resolve(web3);
+  }
+}
 
-export default getWeb3;
+export default () => {
+  return new Promise((resolve, reject) => {
+    // Wait for loading completion to avoid race conditions with web3 injection timing.
+    window.addEventListener("load", () => {
+      getWeb3(resolve, reject);
+    });
+
+    // If document is already loaded, no race conditions are possible.
+    if (document.readyState === 'complete') {
+      getWeb3(resolve, reject);
+    }
+  });
+};
