54533def5eee84b35fa23cdc7c4637c9b8aa4706
Andrew Kim <akinhwan@gmail.com>
2019-05-12 09:42:27 -0400

    remove if web3, remove pricing and CTA sections

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 import "./App.css";
 import { MDBBtn } from "mdbreact";
 import withWeb3 from "./utils/withWeb3";
+import feature1 from "./dist/images/feature-icon-01.svg";
+import feature2 from "./dist/images/feature-icon-02.svg";
+import feature3 from "./dist/images/feature-icon-03.svg";
+import feature4 from "./dist/images/feature-icon-04.svg";
+import feature5 from "./dist/images/feature-icon-05.svg";
+import feature6 from "./dist/images/feature-icon-06.svg";
 
 class App extends Component {
   constructor(props) {
@@ -59,9 +65,9 @@ class App extends Component {
   };
 
   render() {
-    if (!this.props.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
+    // if (!this.props.web3) {
+    //   return <div>Loading Web3, accounts, and contract...</div>;
+    // }
 
     return (
       <div className="body-wrap">
@@ -70,7 +76,7 @@ class App extends Component {
             <div className="container">
               <div className="hero-inner">
                 <div className="hero-copy">
-                  <h1 className="hero-title mt-0">Landing template for startups</h1>
+                  <h1 className="hero-title mt-0">Enabling trust between buyers and sellers.</h1>
                   <p className="hero-paragraph">Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.</p>
                   <div className="hero-cta">
                     <a className="button button-primary" href="/seller"> Sellers </a>
@@ -96,121 +102,6 @@ class App extends Component {
             </div>
           </section>
 
-          <section className="features section">
-            <div className="container">
-              <div className="features-inner section-inner has-bottom-divider">
-                <div className="features-wrap">
-                  <div className="feature text-center is-revealing">
-                    <div className="feature-inner">
-                      <div className="feature-icon">
-                        <img src="dist/images/feature-icon-01.svg" alt="Feature 01" />
-                      </div>
-                      <h4 className="feature-title mt-24">Be Productive</h4>
-                      <p className="text-sm mb-0">Fermentum posuere urna nec tincidunt praesent semper feugiat nibh. A arcu cursus vitae congue mauris. Nam at lectus urna duis convallis. Mauris rhoncus aenean vel elit scelerisque mauris.</p>
-                    </div>
-                  </div>
-                  <div className="feature text-center is-revealing">
-                    <div className="feature-inner">
-                      <div className="feature-icon">
-                        <img src="dist/images/feature-icon-02.svg" alt="Feature 02" />
-                      </div>
-                      <h4 className="feature-title mt-24">Be Productive</h4>
-                      <p className="text-sm mb-0">Fermentum posuere urna nec tincidunt praesent semper feugiat nibh. A arcu cursus vitae congue mauris. Nam at lectus urna duis convallis. Mauris rhoncus aenean vel elit scelerisque mauris.</p>
-                    </div>
-                  </div>
-                  <div className="feature text-center is-revealing">
-                    <div className="feature-inner">
-                      <div className="feature-icon">
-                        <img src="dist/images/feature-icon-03.svg" alt="Feature 03" />
-                      </div>
-                      <h4 className="feature-title mt-24">Be Productive</h4>
-                      <p className="text-sm mb-0">Fermentum posuere urna nec tincidunt praesent semper feugiat nibh. A arcu cursus vitae congue mauris. Nam at lectus urna duis convallis. Mauris rhoncus aenean vel elit scelerisque mauris.</p>
-                    </div>
-                  </div>
-                  <div className="feature text-center is-revealing">
-                    <div className="feature-inner">
-                      <div className="feature-icon">
-                        <img src="dist/images/feature-icon-04.svg" alt="Feature 04" />
-                      </div>
-                      <h4 className="feature-title mt-24">Be Productive</h4>
-                      <p className="text-sm mb-0">Fermentum posuere urna nec tincidunt praesent semper feugiat nibh. A arcu cursus vitae congue mauris. Nam at lectus urna duis convallis. Mauris rhoncus aenean vel elit scelerisque mauris.</p>
-                    </div>
-                  </div>
-                  <div className="feature text-center is-revealing">
-                    <div className="feature-inner">
-                      <div className="feature-icon">
-                        <img src="dist/images/feature-icon-05.svg" alt="Feature 05" />
-                      </div>
-                      <h4 className="feature-title mt-24">Be Productive</h4>
-                      <p className="text-sm mb-0">Fermentum posuere urna nec tincidunt praesent semper feugiat nibh. A arcu cursus vitae congue mauris. Nam at lectus urna duis convallis. Mauris rhoncus aenean vel elit scelerisque mauris.</p>
-                    </div>
-                  </div>
-                  <div className="feature text-center is-revealing">
-                    <div className="feature-inner">
-                      <div className="feature-icon">
-                        <img src="dist/images/feature-icon-06.svg" alt="Feature 06" />
-                      </div>
-                      <h4 className="feature-title mt-24">Be Productive</h4>
-                      <p className="text-sm mb-0">Fermentum posuere urna nec tincidunt praesent semper feugiat nibh. A arcu cursus vitae congue mauris. Nam at lectus urna duis convallis. Mauris rhoncus aenean vel elit scelerisque mauris.</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </section>
-
-          <section className="pricing section">
-            <div className="container-sm">
-              <div className="pricing-inner section-inner">
-                <div className="pricing-header text-center">
-                  <h2 className="section-title mt-0">Unlimited for all</h2>
-                  <p className="section-paragraph mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut ad quis nostrud.</p>
-                </div>
-                <div className="pricing-tables-wrap">
-                  <div className="pricing-table">
-                    <div className="pricing-table-inner is-revealing">
-                      <div className="pricing-table-main">
-                        <div className="pricing-table-header pb-24">
-                          <div className="pricing-table-price"><span className="pricing-table-price-currency h2">$</span><span className="pricing-table-price-amount h1">49</span><span className="text-xs">/month</span></div>
-                        </div>
-                        <div className="pricing-table-features-title text-xs pt-24 pb-24">What you will get</div>
-                        <ul className="pricing-table-features list-reset text-xs">
-                          <li>
-                            <span>Lorem ipsum dolor sit nisi</span>
-                          </li>
-                          <li>
-                            <span>Lorem ipsum dolor sit nisi</span>
-                          </li>
-                          <li>
-                            <span>Lorem ipsum dolor sit nisi</span>
-                          </li>
-                          <li>
-                            <span>Lorem ipsum dolor sit nisi</span>
-                          </li>
-                        </ul>
-                      </div>
-                      <div className="pricing-table-cta mb-8">
-                        <MDBBtn className="button button-primary button-shadow button-block" onClick={this.handleWholesaler}>
-                          Pre order now
-                        </MDBBtn>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </section>
-
-          <section className="cta section">
-            <div className="container">
-              <div className="cta-inner section-inner">
-                <h3 className="section-title mt-0">Still not convinced on buying?</h3>
-                <div className="cta-cta">
-                  <a className="button button-primary button-wide-mobile" href="/">Get in touch</a>
-                </div>
-              </div>
-            </div>
-          </section>
         </main>
       </div>
     );
