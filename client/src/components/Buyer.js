17a63339f4f5203fedf764390346c63d9c9ab732
Andrew Kim <akinhwan@gmail.com>
2019-05-11 23:27:52 -0400

    table bootstrapped

--- client/src/components/Buyer.js
+++ client/src/components/Buyer.js
@@ -6,6 +6,12 @@ import {
   MDBCardHeader,
   MDBContainer,
 } from "mdbreact";
+import active from '../dist/images/active.png'
+import failed from '../dist/images/failed.png'
+import passing from '../dist/images/passing.png'
+import pend from '../dist/images/pend.png'
+import pending from '../dist/images/pending.png'
+import success from '../dist/images/success.png'
 
 class Buyer extends Component {
   state = {
@@ -24,13 +30,20 @@ class Buyer extends Component {
               <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
             </div>
               <input type="email" id="defaultLoginFormEmail" class="form-control mb-4" placeholder="Address"/>
-              <input type="email" id="defaultLoginFormEmail" class="form-control mb-4" placeholder="City"/>
-              <div class="input-group mb-3">
+              <input type="email" id="defaultLoginFormEmail" class="w-50 form-control mb-4" placeholder="City"/>
+              <div class="input-group mb-3 w-50">
               <select class="browser-default custom-select">
                 <option selected>State</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
+                <option value="1">Virginia</option>
+                <option value="2">New York</option>
+                <option value="3">Pennsylvania</option>
+                <option value="3">California</option>
+                <option value="3">Texas</option>
+                <option value="3">Alaska</option>
+                <option value="3">Florida</option>
+                <option value="3">Oregon</option>
+                <option value="3">Nevada</option>
+                <option value="3">Oklahoma</option>
               </select>
               </div>
               <div class="input-group mb-3">  
@@ -41,11 +54,10 @@ class Buyer extends Component {
             </MDBBtn>
           </div>
         </MDBCard>
-        <MDBCard className="mx-auto float-none white z-depth-1 w-75 p-3">
-        <div class="card card-cascade narrower">
+        <div class="card card-cascade narrower w-75 mx-auto float-none">
 
           <div
-            class="view view-cascade gradient-card-header blue-gradient narrower py-2 mx-4 mb-3 d-flex justify-content-between align-items-center">
+            class="view view-cascade gradient-card-header blue-gradient narrower d-flex justify-content-between align-items-center">
 
             <div>
               <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2">
@@ -75,7 +87,7 @@ class Buyer extends Component {
           <div class="px-4">
 
             <div class="table-wrapper table-responsive">
-              <table class="table table-hover mb-0">
+              <table class="table table-sm table-hover mb-0">
                 <thead>
                   <tr>
                     <th>
@@ -83,32 +95,22 @@ class Buyer extends Component {
                       <label class="form-check-label" for="checkbox" class="mr-2 label-table"></label>
                     </th>
                     <th class="th-lg">
-                      <a>First Name
+                      <a>Name
                         <i class="fas fa-sort ml-1"></i>
                       </a>
                     </th>
                     <th class="th-lg">
-                      <a href="">Last Name
+                      <a href="">Amount
                         <i class="fas fa-sort ml-1"></i>
                       </a>
                     </th>
                     <th class="th-lg">
-                      <a href="">Username
+                      <a href="">Address
                         <i class="fas fa-sort ml-1"></i>
                       </a>
                     </th>
                     <th class="th-lg">
-                      <a href="">Username
-                        <i class="fas fa-sort ml-1"></i>
-                      </a>
-                    </th>
-                    <th class="th-lg">
-                      <a href="">Username
-                        <i class="fas fa-sort ml-1"></i>
-                      </a>
-                    </th>
-                    <th class="th-lg">
-                      <a href="">Username
+                      <a href="">Status
                         <i class="fas fa-sort ml-1"></i>
                       </a>
                     </th>
@@ -121,23 +123,33 @@ class Buyer extends Component {
                       <label class="form-check-label" for="checkbox1" class="label-table"></label>
                     </th>
                     <td>Mark</td>
-                    <td>Otto</td>
                     <td>@mdo</td>
                     <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
+                    <td>
+                      <img class="status-step-icon" src={pend}></img>
+                    </td>
                   </tr>
+                  
                   <tr>
-                    <th scope="row">
-                      <input class="form-check-input" type="checkbox" id="checkbox2"/>
-                      <label class="form-check-label" for="checkbox2" class="label-table"></label>
-                    </th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
+                    {/* <div> */}
+                      {/* <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample"
+                        aria-expanded="false" aria-controls="collapseExample">
+                        Button with data-target
+                      </button> */}
+                      <th scope="row" data-toggle="collapse" data-target="#collapseExample"
+                        aria-expanded="false" aria-controls="collapseExample">
+                        <input class="form-check-input" type="checkbox" id="checkbox2"/>
+                        <label class="form-check-label" for="checkbox2" class="label-table"></label>
+                      </th>
+                      <td>Jacob</td>
+                      <td>@fat</td>
+                      <td>Jacob</td>
+                      <td><img class="status-step-icon" src={active}></img></td>
+                    {/* </div> */}
+
+                    <div class="collapse" id="collapseExample">
+                      
+                    </div>
                   </tr>
                   <tr>
                     <th scope="row">
@@ -145,11 +157,9 @@ class Buyer extends Component {
                       <label class="form-check-label" for="checkbox3" class="label-table"></label>
                     </th>
                     <td>Larry</td>
-                    <td>the Bird</td>
                     <td>@twitter</td>
                     <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
+                    <td><img class="status-step-icon" src={failed}></img></td>
                   </tr>
                   <tr>
                     <th scope="row">
@@ -157,11 +167,9 @@ class Buyer extends Component {
                       <label class="form-check-label" for="checkbox4" class="label-table"></label>
                     </th>
                     <td>Paul</td>
-                    <td>Topolski</td>
                     <td>@P_Topolski</td>
                     <td>Paul</td>
-                    <td>Topolski</td>
-                    <td>@P_Topolski</td>
+                    <td><img class="status-step-icon" src={passing}></img></td>
                   </tr>
                   <tr>
                     <th scope="row">
@@ -169,11 +177,9 @@ class Buyer extends Component {
                       <label class="form-check-label" for="checkbox5" class="label-table"></label>
                     </th>
                     <td>Larry</td>
-                    <td>the Bird</td>
                     <td>@twitter</td>
                     <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
+                    <td><img class="status-step-icon" src={success}></img></td>
                   </tr>
                 </tbody>
               </table>
@@ -182,7 +188,6 @@ class Buyer extends Component {
           </div>
 
         </div>
-        </MDBCard>
       </MDBContainer>
     );
   }
